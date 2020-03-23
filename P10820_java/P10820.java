ac860f078b7f01a6371dd6b37bc0429114151918
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-03-24 02:14:33 +0900

    upload

---
+++ P10820_java/P10820.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        StringBuilder result = new StringBuilder();
+
+        String str = br.readLine();
+        int small=0,big=0,num=0,blank=0;
+
+        for(int i=0 ; i < str.length();i++){
+
+        }
+
+
+
+    }
+}
