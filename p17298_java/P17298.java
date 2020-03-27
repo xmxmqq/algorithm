7303a4d360b7bd2d6d0646ec58d6d9e3cfc4b847
조승현 <49122299+wjrmffldrhrl@users.noreply.github.com>
2020-03-27 22:57:50 +0900

    all merge

--- p17298_java/P17298.java
+++ p17298_java/P17298.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -11,8 +10,8 @@ class Main {
         BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
         StringBuilder result = new StringBuilder();
         Stack<Integer> A = new Stack<Integer>();
-        Stack<Integer> B = new Stack<Integer>();
-        Stack<Integer> current = new Stack<Integer>();
+        Stack<Integer> right_num = new Stack<Integer>();
+        Stack<Integer> left_num = new Stack<Integer>();
         int N;
         int EOF=-1;
         
@@ -29,30 +28,33 @@ class Main {
 
         }
         
-        
-        while(!A.isEmpty()){
-            current = (Stack<Integer>) B.clone();
 
-            int target = A.pop();
+        for(int i = 0 ; i < N ; i++){//수열의 길이만큼 반복한다
+            
+            int target = A.pop();// 스택에서 수열을 꺼냄
+            
 
             EOF = -1;
-            while(!current.isEmpty()){
-                int current_num =   current.pop();
-
-                if(current_num>target){
-                    EOF = current_num;
+            while(!right_num.isEmpty()){//target의 오른쪽 수를 저장할 스택이 빌때까지
+                int rn = right_num.pop();//오른쪽 수를 스택에서 꺼낸다
+                left_num.push(rn);
+                if(target < rn){
+                    EOF = rn;
                     break;
                 }
-                
-                EOF = -1;
+            }
+            while(!left_num.isEmpty()){
+                right_num.push(left_num.pop());//꺼냈던 수를 다시 넣어줌
             }
 
-            result.insert(0," "+EOF);
-            B.push(target);
-     
-
+            result.insert(0,EOF+" ");
+            
+            right_num.push(target);
         }
-        
+
+
+
+
         System.out.println(result.toString());
 
 
