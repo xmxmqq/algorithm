e5f0722471e3efe93afd2796b0bc1ee9d8d2749d
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-07-01 10:41:46 +0900

    p10820 solve

--- P10820_java/P10820.java
+++ P10820_java/P10820.java
@@ -1,21 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+package P10820_java;
+
+import java.io.*;
 import java.util.StringTokenizer;
+
 
 class Main {
     public static void main(String args[]) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-        StringBuilder result = new StringBuilder();
+ 
 
-        String str = br.readLine();
-        int small=0,big=0,num=0,blank=0;
+        String str = "";
+        while((str = br.readLine()) != null && str.length() != 0){
+            
+            sb = new StringBuilder();
+            int small=0, big=0, num=0, blank=0;
 
-        for(int i=0 ; i < str.length();i++){
 
+            for(int i = 0 ; i < str.length() ; i++){
+                char target = str.charAt(i);
+                
+                if(target == ' ')
+                    blank++;
+                else if(target >= '0' && target <= '9')
+                    num++;
+                else if(target >= 'A' && target <= 'Z')
+                    big++;
+                else if(target >= 'a' && target <= 'z')
+                    small++;
+            }
+            System.out.println(small + " " + big + " " + num + " " + blank);
         }
-
 
 
     }
