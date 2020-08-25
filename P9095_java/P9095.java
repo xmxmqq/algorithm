bf517e5206a0b464a6ef8ba58563ceee9f2e1d59
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-26 00:19:13 +0900

    p9095 can't solve' '

---
+++ P9095_java/P9095.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+  public static void main(String args[])throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+    
+    int T = Integer.parseInt(br.readLine());
+
+    int[] oneTwoThree = new int[10];
+
+    for(int i = 1 ; i <= 10 ; i++) {
+
+    }
+
+
+
+
+    for(int i = 0 ; i < T ; i++ ) {
+        int target = Integer.parseInt(br.readLine());
+        
+    }
+    
+  }
+
+}
