c7e49b2e29301019dd0569e89109a4d22d360b53
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-09-29 20:47:24 +0900

    P11727 not solved

---
+++ P11727_java/P11727.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+  public static void main(String args[])throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+    StringBuilder result = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    
+  }
+
+}
