e95b6bfc7194246ebd6cb08b687fb1c8881ecba1
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-10-01 14:33:30 +0900

    p11727 solved

--- solved/P11727_java/P11727.java
+++ solved/P11727_java/P11727.java
@@ -7,10 +7,18 @@ class Main{
     BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
     
     int n = Integer.parseInt(br.readLine());
+    
+    int[] F = new int[1000];
+    F[0] = 1;
+    F[1] = 3;
 
-    while(true) {
-      
+    for(int i = 2 ; i < n ; i++) {
+      F[i] = (F[i - 1] + (F[i - 2]* 2) ) % 10007;
     }
+
+    System.out.println(F[n - 1]);
+
+    
    
   }
 
